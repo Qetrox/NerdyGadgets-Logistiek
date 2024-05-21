46d5742bda42531ff41dd59f16e74e1a710a6c81
Qetrox <85188865+Qetrox@users.noreply.github.com>
2024-05-21 13:56:59 +0200

    Backend database connectie, geimplementeerd op test API endpoint

--- backend/src/main/java/nl/nerdygadgets/webserver/handlers/TestHandler.java
+++ backend/src/main/java/nl/nerdygadgets/webserver/handlers/TestHandler.java
@@ -2,8 +2,14 @@ package nl.nerdygadgets.webserver.handlers;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import nl.nerdygadgets.Main;
+import nl.nerdygadgets.util.DatabaseConnector;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Een voorbeeld handler
@@ -14,15 +20,42 @@ public class TestHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
 
-        // Wat we terug sturen
-        String res = "NerdyGadgets is cool";
+        DatabaseConnector conn = Main.getDatabaseConnection();
 
-        // Code 200 om aan te geven dat het OK is, en de lengte van wat we gaan terugsturen
-        exchange.sendResponseHeaders(200, res.length());
+        ResultSet rs = null;
+        Statement stmt = null;
+        Connection connection = null;
 
-        // Stuur alle data
-        exchange.getResponseBody().write(res.getBytes());
-        exchange.getResponseBody().flush();
-        exchange.getResponseBody().close();
+        try {
+            connection = conn.getConnection();
+            stmt = connection.createStatement();
+
+            // Use the database
+            conn.execute("USE u127250p176374_nerdygadgets");
+
+            // Query the database
+            rs = conn.query("SELECT * FROM brand");
+
+            StringBuilder res = new StringBuilder();
+
+            while (rs.next()) {
+                res.append(rs.getString("brandName")).append(" ");
+            }
+
+
+
+            // Code 200 om aan te geven dat het OK is, en de lengte van wat we gaan terugsturen
+            exchange.sendResponseHeaders(200, res.length());
+
+            // Stuur alle data
+            exchange.getResponseBody().write(res.toString().getBytes());
+            exchange.getResponseBody().flush();
+            exchange.getResponseBody().close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            DatabaseConnector.CloseVars(stmt, rs, connection);
+        }
     }
 }
