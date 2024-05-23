9e2ecccbf2fa89092118fe1a07915f5e8a007daa
tarikt <thegreattarikt@gmail.com>
2024-05-24 01:35:07 +0200

    begin van een tabel

--- manager-frontend/src/main/java/nl/nerdygadgets/Manager_overzicht.java
+++ manager-frontend/src/main/java/nl/nerdygadgets/Manager_overzicht.java
@@ -1,24 +1,46 @@
 package nl.nerdygadgets;
 
-import java.awt.FlowLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
+import javax.swing.*;
 public class Manager_overzicht {
-    public static void main(String s[]) {
-        JFrame frame = new JFrame("JFrame Example");
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        JLabel label = new JLabel("JFrame By Example");
-        JButton button = new JButton();
-        button.setText("Button");
-        panel.add(label);
-        panel.add(button);
-        frame.add(panel);
-        frame.setSize(1920, 1080);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+    // frame
+    JFrame f;
+    // Table
+    JTable j;
+
+    // Constructor
+    Manager_overzicht()
+    {
+        // Frame initialization
+        f = new JFrame();
+
+        // Frame Title
+        f.setTitle("JTable Example");
+
+        // Data to be displayed in the JTable
+        String[][] data = {
+                { "ORDER 4343234 ","Alberto " },
+                { "ORDER 4343235","Henk"}
+        };
+
+        // Column Names
+        String[] columnNames = { "Order", "Bezorger"};
+
+        // Initializing the JTable
+        j = new JTable(data, columnNames);
+        j.setBounds(30, 40, 200, 300);
+
+        // adding it to JScrollPane
+        JScrollPane sp = new JScrollPane(j);
+        f.add(sp);
+        // Frame Size
+        f.setSize(500, 200);
+        // Frame Visible = true
+        f.setVisible(true);
     }
-}  
+
+    // Driver  method
+    public static void main(String[] args)
+    {
+        new Manager_overzicht();
+    }
+}
